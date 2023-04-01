49c0e2010d9064a9e0987aa08c6a692fe6860497
Kerri Knier <kerriknier@gmail.com>
2023-04-01 22:43:16 +0100

    serialize response

---
+++ src/backend/blog_lambda.py
@@ -0,0 +1,38 @@
+import json
+from typing import Optional
+
+
+class Response:
+    def __init__(self):
+        self.status_code = 200
+        self.headers = {
+            "content-type": "application/json"
+        }
+        self.is_base64_encoded = False
+        self.multi_value_headers = {
+            "X-Custom-Header": ["My value", "My other value"]
+        }
+        self.body = ""
+
+
+def lambda_handler(event: dict, ctx) -> str:
+    print(event)
+    print(ctx)
+
+    query_params: Optional[dict] = event.get("queryStringParameters")
+    print(query_params)
+
+    value = ""
+    if type(query_params) is dict:
+        value = query_params.get('key')
+        print(value)
+
+    response = Response()
+
+    response.body = {
+        "key1": "value1",
+        "key2": "value2",
+        "qp": value
+    }
+
+    return json.dumps(response,  default=vars)
