2a2a2e49f256d61957afb0432410802325a0b4c6
Kerri Knier <kerriknier@gmail.com>
2023-04-01 22:07:13 +0100

    attempt processing of event in lambda

--- src/backend/lambda.py
+++ src/backend/lambda.py
@@ -1,10 +1,28 @@
-import pandas as pd
+class Response:
+    statusCode = 200
+    headers = {
+        "content-type": "application/json"
+    }
+    isBase64Encoded = False
+    multiValueHeaders = {
+        "X-Custom-Header": ["My value", "My other value"]
+    }
+    body = ""
 
 
 def lambda_handler(event, ctx) -> None:
-    print(event)
-    print(ctx)
-    d = {'col1': [1, 2], 'col2': [3, 4]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Done')
+    query_params = event['queryParameters']
+    print(query_params)
+
+    value = ""
+    if type(query_params) is dict:
+        print(query_params["key"])
+        value = query_params["key"]
+
+    response = Response()
+
+    response.body = {
+        "key1": "value1",
+        "key2": "value2",
+        "qp": value
+    }
