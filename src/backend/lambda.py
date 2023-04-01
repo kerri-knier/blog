74ee42f8dbd054caad76736a0a05bd7e6f84c998
Kerri Knier <kerriknier@gmail.com>
2023-04-01 21:41:53 +0100

    testing lambdas

--- src/backend/lambda.py
+++ src/backend/lambda.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 
-def lambda_handler(event, context):
-    print("executing lambda")
+def lambda_handler(event, ctx) -> None:
+    print(event)
+    print(ctx)
     d = {'col1': [1, 2], 'col2': [3, 4]}
     df = pd.DataFrame(data=d)
     print(df)
