2a9cfb709b1c08f0934e0f2cabdece553965b8b3
Kerri Knier <kerriknier@gmail.com>
2023-04-01 23:25:50 +0100

    add get and post /post endpoints

--- src/backend/blog_lambda.py
+++ src/backend/blog_lambda.py
@@ -1,5 +1,4 @@
-import json
-from typing import Optional
+from humps import camelize
 
 
 class Response:
@@ -14,25 +13,71 @@ class Response:
         }
         self.body = ""
 
+    def to_json_dict(self) -> dict:
+        return camelize(vars(self))
+
 
 def lambda_handler(event: dict, ctx) -> dict:
     print(event)
     print(ctx)
 
-    query_params: Optional[dict] = event.get("queryStringParameters")
-    print(query_params)
+    path: str = event.get("path")
+    verb: str = event.get("httpMethod")
 
-    value = ""
-    if type(query_params) is dict:
-        value = query_params.get('key')
-        print(value)
+    print(verb, path)
+
+    if not path.startswith("/post"):
+        return new_error(404, "not found")
+
+    if verb == "GET":
+        return get_posts()
+
+    if verb == "POST":
+        body = event.get("body")
+
+        if body is None or body == "":
+            return new_error(400, "empty post")
+
+        return create_post(body)
+
+    return new_error(404, "not found")
+
+
+def get_posts() -> dict:
+    response = Response()
+
+    response.body = [
+        "stored first post",
+        "stored second post",
+        "stored third post",
+        "stored fourth and extra long post about anything and everything because now its making sense",
+        "stored fifth post",
+        "stored sixth post",
+        "stored seventh post",
+        "stored eighth post",
+        "stored ninth post",
+        "stored tenth post",
+    ]
+
+    return response.to_json_dict()
+
+
+def create_post(text) -> dict:
+    print("new post:", text)
 
     response = Response()
 
+    response.status_code = 201
+
+    return response.to_json_dict()
+
+
+def new_error(status: int, error: str) -> dict:
+    response = Response()
+
+    response.status_code = status
     response.body = {
-        "key1": "value1",
-        "key2": "value2",
-        "qp": value
+        "error": error
     }
 
-    return vars(response)
+    return response.to_json_dict()
